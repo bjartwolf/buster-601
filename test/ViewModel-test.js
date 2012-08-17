fd7e229cf0c36c01c5f6947dea35ac5d5256cb0e
Bjartwolf <bjartnes@gmail.com>
2012-08-17 15:25:23 +0200

    added test on services

--- test/ViewModel-test.js
+++ test/ViewModel-test.js
@@ -3,5 +3,11 @@ buster.testCase("The ViewModel", {
     "should have one element when initialized": function () {
         var vm = new TaskListViewModel();
         assert.same(vm.tasks().length, 1);
+    },
+    "should call save when save command is called": function () {
+        var vm = new TaskListViewModel();
+        services.save = this.spy();
+        vm.save();
+        assert.calledOnce(services.save);
     }
 });
